97dd794e1c663de1147e83db1630bcd7ba621d89
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-07-24 17:31:21 +0530

    [ADDED]: shubham  Configured Django Admin Panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "role",
+        "is_active",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional Information",
+            {
+                "fields": (
+                    "phone",
+                    "profile_image",
+                    "role",
+                )
+            },
+        ),
+    )
--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'title',
+        'venue',
+        'event_date',
+        'available_seats',
+        'status',
+    )
+
+    list_filter = (
+        'status',
+        'event_date',
+    )
+
+    search_fields = (
+        'title',
+        'venue',
+    )
+
+    ordering = ('-created_at',)
