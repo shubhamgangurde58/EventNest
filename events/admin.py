022345894d74ed7750c50adb664bbf3c8030c008
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-07-25 14:27:05 +0530

    [ADDED]: shubham This code for Event CRUD Complited with Django Admin

--- events/admin.py
+++ events/admin.py
@@ -11,7 +11,6 @@ class EventAdmin(admin.ModelAdmin):
         "organizer",
         "venue",
         "event_date",
-        "start_time",
         "available_seats",
         "status",
     )
@@ -29,60 +28,7 @@ class EventAdmin(admin.ModelAdmin):
     )
 
     ordering = (
-        "event_date",
-        "start_time",
+        "-event_date",
     )
 
     list_per_page = 10
-
-    readonly_fields = (
-        "created_at",
-        "updated_at",
-    )
-
-    fieldsets = (
-        ("Event Information", {
-            "fields": (
-                "title",
-                "description",
-                "category",
-                "organizer",
-            )
-        }),
-
-        ("Schedule", {
-            "fields": (
-                "event_date",
-                "registration_deadline",
-                "start_time",
-                "end_time",
-            )
-        }),
-
-        ("Venue & Seats", {
-            "fields": (
-                "venue",
-                "total_seats",
-                "available_seats",
-            )
-        }),
-
-        ("Banner", {
-            "fields": (
-                "event_banner",
-            )
-        }),
-
-        ("Status", {
-            "fields": (
-                "status",
-            )
-        }),
-
-        ("System Information", {
-            "fields": (
-                "created_at",
-                "updated_at",
-            )
-        }),
-    )
