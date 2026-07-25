963fcd2953ecd00ba07d2df10d4d16fca7444484
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-07-25 11:49:13 +0530

    [MODIFIED]: shubham This code for Professional Django Admin for Events

--- events/admin.py
+++ events/admin.py
@@ -11,14 +11,9 @@ class EventAdmin(admin.ModelAdmin):
         "organizer",
         "venue",
         "event_date",
+        "start_time",
         "available_seats",
         "status",
-    )
-
-    search_fields = (
-        "title",
-        "organizer",
-        "venue",
     )
 
     list_filter = (
@@ -27,4 +22,67 @@ class EventAdmin(admin.ModelAdmin):
         "event_date",
     )
 
-    ordering = ("event_date",)
+    search_fields = (
+        "title",
+        "organizer",
+        "venue",
+    )
+
+    ordering = (
+        "event_date",
+        "start_time",
+    )
+
+    list_per_page = 10
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    fieldsets = (
+        ("Event Information", {
+            "fields": (
+                "title",
+                "description",
+                "category",
+                "organizer",
+            )
+        }),
+
+        ("Schedule", {
+            "fields": (
+                "event_date",
+                "registration_deadline",
+                "start_time",
+                "end_time",
+            )
+        }),
+
+        ("Venue & Seats", {
+            "fields": (
+                "venue",
+                "total_seats",
+                "available_seats",
+            )
+        }),
+
+        ("Banner", {
+            "fields": (
+                "event_banner",
+            )
+        }),
+
+        ("Status", {
+            "fields": (
+                "status",
+            )
+        }),
+
+        ("System Information", {
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+    )
