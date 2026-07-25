5a1edda126f95937a906b28ea7dfa45971d9092f
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-07-25 22:38:54 +0530

    [MODIFIED]: shubham This code for Add Home & Base page to Navigate to home page

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
     path("admin/", admin.site.urls),
-
+    path("", include("dashboard.urls")),
     path("events/", include("events.urls")),
-
 ]
