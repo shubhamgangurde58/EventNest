65b11081ec02e7e33a36493a419b9c1543221b27
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-07-25 14:12:40 +0530

    [ADDED]: shubham This code for setup event crud page and routing

---
+++ events/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path(
+        "add/",
+        views.add_event,
+        name="add_event"
+    ),
+
+    path(
+        "list/",
+        views.event_list,
+        name="event_list"
+    ),
+
+]
--- events/views.py
+++ events/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def add_event(request):
+
+    return render(
+        request,
+        "events/add_event.html"
+    )
+
+
+def event_list(request):
+
+    return render(
+        request,
+        "events/event_list.html"
+    )
