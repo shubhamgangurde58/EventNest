0769eb10cb16edec643f5cf7347a7bbd808223f4
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-08-02 00:09:19 +0530

    [ADDED]: shubham This code for Configured settings for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ----------------------------------------------------
 SECRET_KEY = "django-insecure-+#h&1l5938#a_dtk$u%lpnxcr-#ohxwxqx&321b2@a&r2n6ros"
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # ----------------------------------------------------
@@ -149,6 +153,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # ----------------------------------------------------
 # Media Files (Uploaded Images)
